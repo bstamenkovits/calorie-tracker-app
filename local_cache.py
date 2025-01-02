9d2d2974eba57d6c496fab5444f8cb894421da7e
Bela <none_of_your_bees@wax.com>
2025-01-02 22:17:54 +0100

    feat: implemented LocalCacheInterface class

--- local_cache.py
+++ local_cache.py
@@ -1,15 +1,44 @@
 import os
 import pandas as pd
 
+class LocalCacheInterface:
 
-empty_dfs = {
-    "new_recipe_ingredients": pd.DataFrame(columns=["ingredient", "quantity", "serving"]),
-    "new_recipe_instructions": pd.DataFrame(columns=["instruction"]),
-    "new_recipe_tags": pd.DataFrame(columns=["tag"]),
-}
+    cache_path = "cache/local"
+    empty_dfs = {
+        "new_recipe_info": pd.DataFrame(columns=["description"]),
+        "new_recipe_tags": pd.DataFrame(columns=["tag"]),
+        "new_recipe_ingredients": pd.DataFrame(columns=["ingredient", "quantity", "serving"]),
+        "new_recipe_instructions": pd.DataFrame(columns=["instruction"]),
+    }
 
-def load_from_local_cache(file_name: str) -> pd.DataFrame:
-    file_path = file_name + ".csv"
-    if not os.path.exists(file_path):
-        return empty_dfs[file_name]
-    return pd.read_csv(file_path)
+    def __init__(self):
+        pass
+
+    def load_from_local_cache(self, sheet_name: str) -> pd.DataFrame:
+        file_path = f"{self.cache_path}/{sheet_name}.csv"
+        if not os.path.exists(file_path):
+            return self.empty_dfs[sheet_name]
+        return pd.read_csv(file_path)
+
+    def update_local_cache(self, sheet_name: str, df: pd.DataFrame) -> None:
+        """
+        Update data in local cache with new data.
+
+        Args:
+            sheet_name (str): The name of the sheet in the local cache
+            df (pd.DataFrame): The updated data (old data + new data)
+                to be stored in the local cache
+        """
+        file_path = f"{self.cache_path}/{sheet_name}.csv"
+        df.to_csv(file_path, index=False)
+
+    def clear_local_cache(self, sheet_name: str) -> None:
+        """
+        Remove a sheet in the local cache.
+
+        Args:
+            sheet_name (str): The name of the sheet in the local cache to be removed
+        """
+        file_path = f"{self.cache_path}/{sheet_name}.csv"
+        if os.path.exists(file_path):
+            os.remove(file_path)
